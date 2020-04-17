d2f4a3e1c3652fca9a32c6f0348eb4e82d04780e
Your Name <you@example.com>
2020-04-17 10:04:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from "./App.module.css"
+import {Cards,Chart,CountryPicker} from './components';
+import {fetchData} from './api';
+import covid from './images/image';
+class App extends React.Component
+{
+    state={
+        data:{},
+        country:'',
+
+    }
+    async componentDidMount(){
+        const fetchedData= await fetchData();
+        this.setState({data:fetchedData});
+    }
+    handleCountryChange=async(country)=>{
+        const fetchedData=await fetchData(country);
+        console.log(fetchedData);
+        this.setState({data:fetchedData,country:country});
+    }
+    render(){
+        const {data,country}=this.state;
+        return(
+            <div className={styles.container}>
+                    <img className={styles.image}  src={covid} alt="COVID-19"/>
+                    <Cards data={data}/>
+                    <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                    <Chart data={data} country={country}/>
+            </div>
+        )
+    }
+}
+export default App;
